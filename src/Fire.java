1cb53d7745aa11707cd76a6f0b99d286483f9fe9
uriya <ooria47@gmail.com>
2021-05-31 00:48:27 +0300

    adding fire

---
+++ src/Fire.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Fire extends JLabel {
+    int x_position, y_position;
+    Icon upIcon;
+
+    public Fire() {
+        upIcon = new ImageIcon("Gifs/fire.gif");
+
+    }
+}
